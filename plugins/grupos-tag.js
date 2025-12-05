cd212c547494c80bfc4f785940a2738454589f08
Jarednnnn <patylachina3@gmail.com>
2025-12-05 04:11:33 -0500

    Update grupos-tag.js

--- plugins/grupos-tag.js
+++ plugins/grupos-tag.js
@@ -8,37 +8,34 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
     const msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, {[m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : {text: '' || c}}, {quoted: m, userJid: conn.user.id}), text || q.text, conn.user.jid, {mentions: users});
     await conn.relayMessage(m.chat, msg.message, {messageId: msg.key.id});
   } catch {
-    /**
-[ By @NeKosmic || https://github.com/NeKosmic/ ]
-**/
-
     const users = participants.map((u) => conn.decodeJid(u.id));
     const quoted = m.quoted ? m.quoted : m;
     const mime = (quoted.msg || quoted).mimetype || '';
     const isMedia = /image|video|sticker|audio/.test(mime);
     const more = String.fromCharCode(8206);
     const masss = more.repeat(850);
-    const htextos = `${text ? text : '*🌟 Debes enviar un texto para hacer un tag.*'}`;
+    const htextos = `${text ? text : '┏━━━━━━━━━━━━━━━━━━━━━┓\n┃  ⓘ ORDEN INCOMPLETA ┃\n┗━━━━━━━━━━━━━━━━━━━━━┛\n\n> Especifique el mensaje para la convocatoria.\n\n"."'}`;
+    
     if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
       var mediax = await quoted.download?.();
-      conn.sendMessage(m.chat, {image: mediax, mentions: users, caption: htextos, mentions: users}, {quoted: m});
+      conn.sendMessage(m.chat, {image: mediax, mentions: users, caption: htextos}, {quoted: m});
     } else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
       var mediax = await quoted.download?.();
       conn.sendMessage(m.chat, {video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos}, {quoted: m});
     } else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
       var mediax = await quoted.download?.();
-      conn.sendMessage(m.chat, {audio: mediax, mentions: users, mimetype: 'audio/mpeg', fileName: `Hidetag.mp3`}, {quoted: m});
+      conn.sendMessage(m.chat, {audio: mediax, mentions: users, mimetype: 'audio/mpeg', fileName: `Convocatoria.mp3`}, {quoted: m});
     } else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
       var mediax = await quoted.download?.();
       conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, {quoted: m});
     } else {
-      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: imagen1, sourceUrl: md }}}}}, {});
+      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: global.icono, sourceUrl: 'https://github.com/Jared'}}}}}, {});
     }
   }
 };
-handler.help = ['tag'];
+handler.help = ['tag', 'convocar'];
 handler.tags = ['grupo'];
-handler.command = ['tag'];
+handler.command = ['tag', 'convocar', 'convocatoria'];
 handler.group = true;
 handler.admin = true;
 export default handler;
